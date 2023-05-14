fc02f3fb67447c7d847ae3a953c58137158e2c04
ahmadmm01 <ahmad.musthafa.ft20@mail.umy.ac.id>
2023-05-15 00:35:09 +0700

    add edit content

--- lib/view/kategoriBarang/edit_kategori_barang.dart
+++ lib/view/kategoriBarang/edit_kategori_barang.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
 
 class EditKategoriBarang extends StatefulWidget {
   const EditKategoriBarang({super.key});
@@ -11,6 +10,35 @@ class EditKategoriBarang extends StatefulWidget {
 class _EditKategoriBarangState extends State<EditKategoriBarang> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Edit Kategori Barang'),
+      ),
+      body: Form(
+        child: ListView(
+          padding: const EdgeInsets.all(16.0),
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                hintText: 'Nama Kategori Barang',
+                labelText: 'Nama Kategori Barang',
+              ),
+              onChanged: (value) {},
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Nama Kategori is required';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Simpan'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
